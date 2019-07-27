7718d47c8a618f19d169fba0b1ae830815c2d7eb
anantm007 <anant.mathur007@gmail.com>
2019-07-27 11:36:56 +0530

    update landing page

--- routes/edumonk.js
+++ routes/edumonk.js
@@ -4,13 +4,17 @@ const Register = require('../Models/registermodel');
 const Query = require('../Models/querymodel');
 const passwordHash = require('password-hash');
 const Challenge = require("../Models/challengemodel");
-
+const Startup = require("../Models/startupmodel");
 
 router.get('/',async(req,res)=>{
    const challenge = await Challenge.find().sort({'_id' : -1}).limit(10);
+   const startup = await Startup.find().sort({'_id': -1}).limit(10);
+   if(startup)
+   console.log(startup);
    if(challenge){
     res.render('landing',{
-        Challenge : challenge
+        Challenge : challenge,
+        Startup: startup
     });}});
 
 module.exports = router;
